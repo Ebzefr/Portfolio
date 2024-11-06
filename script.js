e2fef1f7cddeedc318ae3bf243f2070adb0e644b
Ebzefr <151416411+Ebzefr@users.noreply.github.com>
2024-11-06 18:06:12 +0000

    New commit 3

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icons = document.querySelector(".ham-icons");
+    menu.classList.toggle("open"); 
+    icons.classList.toggle("open"); 
+
+}
